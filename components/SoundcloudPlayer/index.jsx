8fd315c7c83b71e99b504eb38dc6f71301122470
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-08-08 01:27:28 +0300

    add Timer at SoundcloudPlayer (need fixings)

--- components/SoundcloudPlayer/index.jsx
+++ components/SoundcloudPlayer/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PlayButton, Progress } from 'react-soundplayer/components';
+import { PlayButton, Timer, Progress } from 'react-soundplayer/components';
 import { SoundPlayerContainer } from 'react-soundplayer/addons';
 
 const clientId = '305b2f7948fb888a0222751f4ca59398';
@@ -16,6 +16,7 @@ class Player extends React.Component {
             <PlayButton {...this.props} />
                 <div style={{float:'right',textAlign:'right'}}>
                     <div className='soundcloud-player__track-name'>{ track ? track.title : 'Loading...' }</div>
+                    <Timer duration={ track ? track.duration / 1000 : 0 } currentTime={currentTime} {...this.props} />
                 </div>
                 <Progress {...this.props} />
             </div>
