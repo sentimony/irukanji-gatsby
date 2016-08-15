37b3f09694c3b9828a8a2e81e55ee4760dcf5b51
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-08-15 23:42:24 +0300

    upd MixcloudPlayer component

--- components/MixcloudPlayer/index.jsx
+++ components/MixcloudPlayer/index.jsx
@@ -6,7 +6,7 @@ class MixcloudPlayer extends React.Component {
     render() {
         return (
             <div className='mixcloud-player'>
-                <iframe className="mixcloud-player__iframe" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Flunecell_sets%2Flunecellfractiliannewyears2014%2F&hide_cover=1&autoplay=1" />
+                <iframe className="mixcloud-player__iframe" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Flunecell_sets%2Flunecellfractiliannewyears2014%2F&hide_cover=1&autoplay=0" />
             </div>
         );
     }
