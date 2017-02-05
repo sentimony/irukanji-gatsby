2446befcd482f9ccb5cf4b65df89ba3cae89b28f
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-02-05 21:25:51 +0200

    upd Footer component

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { prefixLink } from 'gatsby-helpers'
-import { config } from 'config'
 
 import './style.scss'
 import iconEagle from '../../static/img/svg-icons/native-american-eagle.svg'
@@ -9,12 +8,12 @@ class Footer extends React.Component {
     render() {
         return (
             <div className='footer'>
-                <div className='footer__copyright'>2008 - 2017 © { config.siteTitle }</div>
+                <div className='footer__copyright'>2008 - 2017 © Irukanji</div>
                 <div className='footer__powered'>
                     <span className='footer__powered-development'>Web development by</span>
-                    <a href={ config.poweredUrl } className='footer__powered-link' target='_blank'>
+                    <a href='https://ihororlovskyi.com' className='footer__powered-link' target='_blank'>
                         <img className='footer__powered-link-svg' src={ prefixLink(iconEagle) }/>
-                        <span className='footer__powered-link-text'>{ config.poweredTitle }</span>
+                        <span className='footer__powered-link-text'>Ihor Orlovskyi</span>
                     </a>
                 </div>
             </div>
