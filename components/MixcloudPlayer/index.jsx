3965e2cc174c2599134d676430fa4f23812c5215
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2020-06-14 20:24:26 +0300

    Update index.jsx

--- components/MixcloudPlayer/index.jsx
+++ components/MixcloudPlayer/index.jsx
@@ -6,7 +6,8 @@ class MixcloudPlayer extends React.Component {
     render() {
         return (
             <div className='mixcloud-player'>
-                <iframe className="mixcloud-player__iframe" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Flunecell_sets%2Flunecellfractiliannewyears2014%2F&hide_cover=1&autoplay=0" />
+<!--                 <iframe className="mixcloud-player__iframe" src="https://www.mixcloud.com/widget/iframe/?feed=https%3A%2F%2Fwww.mixcloud.com%2Flunecell_sets%2Flunecellfractiliannewyears2014%2F&hide_cover=1&autoplay=0" /> -->
+                <iframe className="mixcloud-player__iframe" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2Firukanji%2Fgood-murning-morning-zenonesque-psytrance-djset-by-irukanji%2F" />
             </div>
         );
     }
